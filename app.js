a33d4cff72ff391b177a9cf6b31630c7e7df4bb9
gz <zhouguanzong@gmail.com>
2023-01-15 00:17:22 +0800

    Add play single song feature

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
-require('dotenv').config();
+require("dotenv").config();
 
-var createError = require('http-errors');
-var express = require('express');
-var fs = require('node:fs');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+var createError = require("http-errors");
+var express = require("express");
+var fs = require("node:fs");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
 const { Player, QueryType } = require("discord-player");
 
 const { token } = process.env.DISCORD_TOKEN;
-const client = new Client({ intents: [
-  GatewayIntentBits.Guilds,
-  GatewayIntentBits.GuildMessages,
-  GatewayIntentBits.MessageContent,
-  GatewayIntentBits.GuildMembers,
-  GatewayIntentBits.GuildVoiceStates
-]});
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildVoiceStates,
+  ],
+});
 
 client.commands = new Collection();
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
 
 client.once("ready", () => {
   console.log("Ready!");
@@ -32,84 +36,98 @@ client.once("disconnect", () => {
   console.log("Disconnect!");
 });
 
-const player = new Player(client);
+client.player = new Player(client, {
+  ytdlOptions: {
+    quality: "highestaudio",
+    highWaterMark: 1 << 25,
+  },
+});
 
+client.player.on("trackStart", (queue, track) => {
+  queue.metadata.send(`Playing ${track.title}.`);
+});
+
+client.player.on("trackAdd", (queue, track) => {
+  queue.metadata.send(`Added ${track.title} to the queue.`);
+});
+
+client.player.on("trackEnd", (queue, track) => {
+  queue.metadata.send(`Playing ${track.title}.`);
+  queue.play();
+});
 
 for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	client.commands.set(command.data.name, command);
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  client.commands.set(command.data.name, command);
 }
 
 client.on(Events.MessageCreate, async (message) => {
-  // console.log(message);
   if (!message.author.bot) {
     message.channel.send("deeznuts");
-  const args = message.content.split(" ");
+    const args = message.content.split(" ");
 
-  const voiceChannel = message.member.voice.channel;
-  if (!voiceChannel) {
-    return message.channel.send(
-      "You need to be in a voice channel to play music!"
-    );
-  }
-
-
-
+    const voiceChannel = message.member.voice.channel;
+    if (!voiceChannel) {
+      return message.channel.send(
+        "You need to be in a voice channel to play music!"
+      );
+    }
   }
 });
 
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
 
-	const command = client.commands.get(interaction.commandName);
+  const command = client.commands.get(interaction.commandName);
 
-	if (!command) return;
+  if (!command) return;
 
-	try {
-		await command.execute(interaction, player);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
+  try {
+    await command.execute(interaction, client);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
 });
 
 client.login(token);
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
-
-
 
 module.exports = app;
--- commands/play.js
+++ commands/play.js
@@ -1,36 +1,51 @@
-
-const { SlashCommandBuilder } = require('discord.js');
-const {QueryType} = require('discord-player');
-const ffmpeg = require('ffmpeg-static');
+const { SlashCommandBuilder } = require("discord.js");
+const { QueryType } = require("discord-player");
+const ffmpeg = require("ffmpeg-static");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('play')
-		.setDescription('Plays music')
-        .addStringOption((option) => option.setName('query').setDescription('The song name').setRequired(true)),
-	async execute(interaction, player) {
-		var message = await interaction.deferReply();
-        const query = interaction.options.get("query").value;
-        const searchResult = await player
-            .search(query, {
-                requestedBy: interaction.user,
-                searchEngine: QueryType.AUTO
-            })
-            .catch(() => {});
-        if (!searchResult || !searchResult.tracks.length) return void interaction.followUp({ content: "No results were found!" });
-        const queue = await player.createQueue(interaction.guild, {
-            metadata: interaction.channel
-        });
+  data: new SlashCommandBuilder()
+    .setName("play")
+    .setDescription("Plays music")
+    .addStringOption((option) =>
+      option.setName("query").setDescription("The song name").setRequired(true)
+    ),
+  async execute(interaction, client) {
+    await interaction.deferReply();
+    const query = interaction.options.get("query").value;
 
-        try {
-            if (!queue.connection) await queue.connect(interaction.member.voice.channel);
-        } catch {
-            void player.deleteQueue(interaction.guildId);
-            return void interaction.followUp({ content: "Could not join your voice channel!" });
-        }
+    const searchResult = await client.player
+      .search(query, {
+        requestedBy: interaction.user,
+        searchEngine: QueryType.AUTO,
+      })
+      .catch(() => {});
+    if (!searchResult || !searchResult.tracks.length)
+      return void interaction.followUp({ content: "No results were found!" });
+    const queue =
+      (await client.player.getQueue(interaction.guild)) ??
+      (await client.player.createQueue(interaction.guild, {
+        metadata: interaction.channel,
+      }));
 
-        await interaction.followUp({ content: `⏱ | Loading your ${searchResult.playlist ? "playlist" : "track"}...` });
-        searchResult.playlist ? queue.addTracks(searchResult.tracks) : queue.addTrack(searchResult.tracks[0]);
-        if (!queue.playing) await queue.play();
-	},
+    console.log(client.player.getQueue(interaction.guild));
+    try {
+      if (!queue.connection)
+        await queue.connect(interaction.member.voice.channel);
+    } catch {
+      void client.player.deleteQueue(interaction.guildId);
+      return void interaction.followUp({
+        content: "Could not join your voice channel!",
+      });
+    }
+
+    await interaction.followUp({
+      content: `⏱ | Loading your ${
+        searchResult.playlist ? "playlist" : "track"
+      }...`,
+    });
+    searchResult.playlist
+      ? await queue.addTracks(searchResult.tracks)
+      : await queue.addTrack(searchResult.tracks[0]);
+    if (!queue.playing) await queue.play();
+  },
 };
